82e82836d4078920a96183ec3d8297091744af02
User <User@DEXY8>
2021-02-04 19:28:50 +0300

    Сборщик мусора

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -10,8 +10,30 @@ namespace ConsoleApp20
     class Program
     {
         static void Main(string[] args)
-        {
+        {   // поколения
+            //  0 - 256 Кб
+            //  1 - 2 Мб
+            //  2 - 10 Мб
+            string s1 = new string('*',100);
+            WriteLine(GC.MaxGeneration);
+
+            WriteLine(GC.GetTotalMemory(false));
+            WriteLine("{0: ### ### ### ###}", GC.GetTotalMemory(false));
+            WriteLine("Generation is " + GC.GetGeneration(s1));
+            
+            
+            FillMemory(16);
+            //FillMemory(16);
+
+
+            //GC.Collect();
+            WriteLine("{0: ### ### ### ###}", GC.GetTotalMemory(true));
+            WriteLine("Generation is " + GC.GetGeneration(s1));
             ReadKey();
+        }
+        static void FillMemory(int size)
+        {
+            string sHidden = new string('*', 341 * size);
         }
     }
 }
