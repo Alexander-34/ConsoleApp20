8758e8160c631b9d18716e93dd70d7a9e6652f68
User <User@DEXY8>
2021-02-04 20:04:10 +0300

    Dispose()

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -7,13 +7,41 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp20
 {
+    class Resource
+    {
+        string Name;
+        string s = new string('*', 256 * 341);
+        public Resource(string name)
+        {
+            Name = name;
+        }
+        // protected override void Finalize(string name)
+        ~Resource()
+        {
+            WriteLine(Name + " has been finalized");
+        }
+        /*
+          protected override void Finalize()
+        {
+
+        }
+        */
+        
+    }
     class Program
     {
+        static void CreateResource(string name)
+        {
+
+            WriteLine(name + " has been finalized");
+
+        }
         static void Main(string[] args)
         {   // поколения
             //  0 - 256 Кб
             //  1 - 2 Мб
             //  2 - 10 Мб
+            /*
             string s1 = new string('*',100);
             WriteLine(GC.MaxGeneration);
 
@@ -29,11 +57,26 @@ namespace ConsoleApp20
             //GC.Collect();
             WriteLine("{0: ### ### ### ###}", GC.GetTotalMemory(true));
             WriteLine("Generation is " + GC.GetGeneration(s1));
+            */
+            Resource r = new Resource("01");
+            try
+            {
+                CreateResource("CreateResource ");
+            }
+            finally
+            {
+                r.Dispose();
+                //  r = null;
+            }
+            
+            GC.Collect();
             ReadKey();
         }
         static void FillMemory(int size)
         {
             string sHidden = new string('*', 341 * size);
+            
         }
+        
     }
 }
